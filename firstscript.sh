d7000e86ec8709c4d00078a2714f0bce04da821d
laud <natonish@yahoo.com>
2024-04-08 20:34:29 +0100

    firstscript.sh

---
+++ firstscript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+#echo the statement "start of my first script"
+echo "start of my first script" 
+
+#create a directory called devops 
+mkdir devops 
+
+#change directory into the new folder and list the contents
+cd devops 
+
+#create a file called script.sh in the new folder
+touch script.sh 
+
+#echo the statement "end my first script" 
+echo "end of my first script" 
